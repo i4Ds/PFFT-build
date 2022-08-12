91009660dd70bb26c51e37ff958dbb0c8f40a554
FSchramka <fschramka@users.noreply.github.com>
2022-08-12 15:10:20 +0200

    Update build.sh
    add library links for Macos

--- conda/build.sh
+++ conda/build.sh
@@ -3,6 +3,6 @@
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
 ./bootstrap.sh
-./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc"
+./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc -lfftw3_mpi -lfftw3_omp -lfftw3" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc"
 make
 make install
