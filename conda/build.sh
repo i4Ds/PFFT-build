4bed11f516cb31ad7233a6152ecbd10c2420ee21
Filip Schramka <filip.schramka@fhnw.ch>
2022-08-12 16:36:20 +0200

    added OS switch to build.sh

--- conda/build.sh
+++ conda/build.sh
@@ -3,6 +3,12 @@
 COMPILER_PATH="$BUILD_PREFIX/bin"
 
 ./bootstrap.sh
+
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc"
+elif [[ "$OSTYPE" == "darwin"* ]]; then
 ./configure --prefix=$PREFIX CC="$COMPILER_PATH/mpicc -lfftw3_mpi -lfftw3_omp -lfftw3" F77="$COMPILER_PATH/mpif90" MPICC="$COMPILER_PATH/mpicc"
+fi
+
 make
 make install
